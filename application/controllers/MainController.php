1b7db27da7e4c659b36676f712978e7c1c485be1
Yohandy Chiawanda <yohandych16@gmail.com>
2018-06-21 15:23:18 +0700

    Initial Commit

---
+++ application/controllers/MainController.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MainController extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('pages/Homepage');
+	}
+}
---
+++ application/views/pages/Homepage.php
@@ -0,0 +1,16 @@
+<?php echo doctype('html5');?>
+<html>
+<head>
+    <title>E-Library</title>
+    <?php echo meta('viewport','width=device-width, initial-scale=1.0, maximum-scale=1.0'); ?>
+</head>
+<body class="appear-animate">
+    <!-- Page Loader -->        
+    <div class="page-loader">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+
+    
+</body>
+</html>

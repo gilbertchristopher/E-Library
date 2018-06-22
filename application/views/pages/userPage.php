1197733dd72a87ec801e45af88f7adea41163864
Nathaniel <nathanielsuhardiman@gmail.com>
2018-06-22 13:09:45 +0700

    Updated Search Box in userPage.php

--- application/views/pages/userPage.php
+++ application/views/pages/userPage.php
@@ -86,50 +86,7 @@
                             <li><a>&nbsp;</a></li>
                             <!-- End Divider -->
                             
-                            <!-- Search -->
-                            <!-- <li>
-                                <a href="#" class="mn-has-sub"><i class="fa fa-search"></i> Search</a>
-                                
-                                <ul class="mn-sub">
-                                    
-                                    <li>
-                                        <div class="mn-wrap">
-                                            <form method="post" class="form">
-                                                <div class="search-wrap">
-                                                    <button class="search-button animate" type="submit" title="Start Search">
-                                                        <i class="fa fa-search"></i>
-                                                    </button>
-                                                    <input type="text" class="form-control search-field" placeholder="Search...">
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </li>
-                                    
-                                </ul>
-                                
-                            </li> -->
-                            <!-- End Search -->
                             
-                            <!-- Cart -->
-                            <!-- <li>
-                                <a href="#" class="active"><i class="fa fa-shopping-cart"></i> Cart(1)</a>
-                            </li> -->
-                            <!-- End Cart -->
-                            
-                            <!-- Languages -->
-                            <!-- <li>
-                                <a href="#" class="mn-has-sub">Eng <i class="fa fa-angle-down"></i></a>
-                                
-                                <ul class="mn-sub">
-                                    
-                                    <li><a href="">English</a></li>
-                                    <li><a href="">France</a></li>
-                                    <li><a href="">Germany</a></li>
-                                    
-                                </ul>
-                                
-                            </li> -->
-                            <!-- End Languages -->
                             
                         </ul>
                     </div>
@@ -166,14 +123,57 @@
                 </div>
             </section>
             <!-- End Head Section -->
-            
-            
+
+                            
             <!-- Section -->
             <section class="page-section">
                 <div class="container relative">
+
+                    <h2 class="section-title font-alt mb-70 mb-sm-40">
+                        Book List Search
+                    </h2>
                     
+                    <!-- Row -->
                     <div class="row">
                         
+                        <!-- Col -->
+                        <div class="col-sm-2 mb-40"> 
+                            <!-- Kosongin aja --> 
+                        </div>
+                        <!-- End Col -->
+                        
+                        <!-- Col -->   
+                        <div class="col-sm-8 mb-40">
+                            
+                            <!-- Form -->
+                            <form method="post" action="#" id="form" role="form" class="form">
+
+                                <div class="mb-20 mb-md-10">
+                                    <!-- Search box -->
+                                    <input type="text" name="searchBookList" id="searchBookList" class="input-md form-control" placeholder="Title / Author / Keyword" maxlength="300">
+                                </div>
+                                
+                                <div class="mb-20 mb-md-10 align-center">
+                                    <input type="button" name="btnSearchBookList" id="btnSearchBookList" class="btn btn-mod btn-border btn-large btn-round" value="Search">
+                                </div>
+                                                            
+                            </form>
+							<!-- End Form -->
+                            
+                        </div>
+                        <!-- End Col -->
+                        
+                        <!-- Col -->
+                        <div class="col-sm-2 mb-40">  
+                            <!-- Kosongin aja -->   
+                        </div>
+                        <!-- End Col -->
+                        
+                    </div>
+                    <!-- End Row -->
+                   
+                    <div class="row">
+  
                         <!-- Content Book List -->
                         <div class="col-sm-12">
                             
@@ -275,10 +275,58 @@
                             
                         </div>
                         <!-- End Content Book List-->
+
+                    </div>
+
                         
-                        <!-- Divider -->
-                        <br><br>
-                        <!-- End Divider -->
+                    <!-- Divider -->
+                    <br><br>
+                    <!-- End Divider -->
+
+                    <h2 class="section-title font-alt mb-70 mb-sm-40">
+                        Wait List Search
+                    </h2>
+                    
+                    <!-- Row -->
+                    <div class="row">
+                        
+                        <!-- Col -->
+                        <div class="col-sm-2 mb-40"> 
+                            <!-- Kosongin aja --> 
+                        </div>
+                        <!-- End Col -->
+                        
+                        <!-- Col -->   
+                        <div class="col-sm-8 mb-40">
+                            
+                            <!-- Form -->
+                            <form method="post" action="#" id="form" role="form" class="form">
+
+                                <div class="mb-20 mb-md-10">
+                                    <!-- Search box -->
+                                    <input type="text" name="searchWaitList" id="searchWaitList" class="input-md form-control" placeholder="Title / Author / Keyword" maxlength="300">
+                                </div>
+                                
+                                <div class="mb-20 mb-md-10 align-center">
+                                    <input type="button" name="btnSearchWaitList" id="btnSearchWaitList" class="btn btn-mod btn-border btn-large btn-round" value="Search">
+                                </div>
+                                                            
+                            </form>
+							<!-- End Form -->
+                            
+                        </div>
+                        <!-- End Col -->
+                        
+                        <!-- Col -->
+                        <div class="col-sm-2 mb-40">  
+                            <!-- Kosongin aja -->   
+                        </div>
+                        <!-- End Col -->
+                        
+                    </div>
+                    <!-- End Row -->
+
+                    <div class="row">
 
                         <!-- Content Wait List -->
                         <div class="col-sm-12">

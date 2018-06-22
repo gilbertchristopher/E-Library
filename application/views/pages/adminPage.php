9a28ace54692f3288e55fc0aae982d6a0aa5462a
Nathaniel <nathanielsuhardiman@gmail.com>
2018-06-22 15:10:30 +0700

    Edit 2 Tab Pane adminPage.php

--- application/views/pages/adminPage.php
+++ application/views/pages/adminPage.php
@@ -130,55 +130,16 @@
                     <!-- Nav tabs -->
                     <ul class="nav nav-tabs tpl-alt-tabs font-alt pb-30 pb-sm-0">
                         <li class="active">
-                            <a href="#service-branding" data-toggle="tab">
-                                
-                                <div class="alt-tabs-icon">
-                                    <span class="icon-strategy"></span>
-                                </div>
-                                
-                                Branding
+                            <a href="#manage" data-toggle="tab">
+                                Manage
                             </a>
                         </li>
                         <li>
-                            <a href="#service-web-design" data-toggle="tab">
-                                
-                                <div class="alt-tabs-icon">
-                                    <span class="icon-desktop"></span>
-                                </div>
-                                
-                                Web Design
+                            <a href="#add" data-toggle="tab">
+                                Add
                             </a>
                         </li>
-                        <li>
-                            <a href="#service-graphic" data-toggle="tab">
-                                
-                                <div class="alt-tabs-icon">
-                                    <span class="icon-tools"></span>
-                                </div>
-                                
-                                Graphic Design
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#service-development" data-toggle="tab">
-                                
-                                <div class="alt-tabs-icon">
-                                    <span class="icon-gears"></span>
-                                </div>
-                                
-                                Development
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#service-photography" data-toggle="tab">
-                                
-                                <div class="alt-tabs-icon">
-                                    <span class="icon-camera"></span>
-                                </div>
-                                
-                                Photography
-                            </a>
-                        </li>
+                        
                     </ul>
                     <!-- End Nav tabs -->
                     
@@ -186,7 +147,7 @@
                     <div class="tab-content tpl-tabs-cont">
                         
                         <!-- Service Item -->
-                        <div class="tab-pane fade in active" id="service-branding">
+                        <div class="tab-pane fade in active" id="manage">
                             
                             <div class="section-text">
                                 <div class="row">
@@ -219,7 +180,7 @@
                         <!-- End Service Item -->
                         
                         <!-- Service Item -->
-                        <div class="tab-pane fade" id="service-web-design">
+                        <div class="tab-pane fade" id="add">
                             
                             <div class="section-text">
                                 <div class="row">
@@ -250,100 +211,6 @@
                         </div>
                         <!-- End Service Item -->
                         
-                        <!-- Service Item -->
-                        <div class="tab-pane fade" id="service-graphic">
-                            
-                            <div class="section-text">
-                                <div class="row">
-                                    <div class="col-md-4 mb-md-40 mb-xs-30">
-                                        <blockquote class="mb-0">
-                                            <p>
-                                                Never fall in&nbsp;love with an&nbsp;idea. They&rsquo;re whores. If&nbsp;the one you&rsquo;re with isn&rsquo;t doing the job, there&rsquo;s always another.
-                                            </p>
-                                            <footer>
-                                                Chip Kidd
-                                            </footer>
-                                        </blockquote>
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                        Lorem ipsum dolor sit semper amet, consectetur adipiscing elit. In maximus ligula metus pellentesque
-                                        mattis. Donec vel ultricies purus. Nam dictum sem, ipsum aliquam . Praesent sed nisi eleifend, 
-                                        fermentum orci amet, iaculis libero. 
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                         
-                                        Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Etiam sit amet 
-                                        fringilla lacus. Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id&nbsp;dolor 
-                                        consectetur fermentum volutpat nibh, accumsan purus. 
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <!-- End Service Item -->
-                        
-                        <!-- Service Item -->
-                        <div class="tab-pane fade" id="service-development">
-                            
-                            <div class="section-text">
-                                <div class="row">
-                                    <div class="col-md-4 mb-md-40 mb-xs-30">
-                                        <blockquote class="mb-0">
-                                            <p>
-                                                All that is&nbsp;valuable in&nbsp;human society depends upon the opportunity for development accorded the individual.
-                                            </p>
-                                            <footer>
-                                                Albert Einstein
-                                            </footer>
-                                        </blockquote>
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                        Fusce hendrerit vitae nunc id gravida. Donec euismod quis ante at mattis. Mauris dictum ante sit 
-                                        amet enim interdum semper. Vestibulum odio justo, faucibus et dictum eu, malesuada nec neque.
-                                        Maecenas  volutpat, diam enim sagittis. 
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                        Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Sed id dolor consectetur fermentum 
-                                        volutpat nibh, accumsan purus. Lorem ipsum dolor sit semper amet, consectetur adipiscing elit. 
-                                        Inmed maximus ligula metus pellentesque.
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <!-- End Service Item -->
-                        
-                        <!-- Service Item -->
-                        <div class="tab-pane fade" id="service-photography">
-                            
-                            <div class="section-text">
-                                <div class="row">
-                                    <div class="col-md-4 mb-md-40 mb-xs-30">
-                                        <blockquote class="mb-0">
-                                            <p>
-                                                Photography is&nbsp;the simplest thing in&nbsp;the world, but it&nbsp;is&nbsp;incredibly 
-                                                complicated to&nbsp;make it&nbsp;really work.
-                                            </p>
-                                            <footer>
-                                                Martin Parr
-                                            </footer>
-                                        </blockquote>
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                        Donec vel ultricies purus. Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. 
-                                        Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. 
-                                        Praesent sed nisi eleifend, fermentum orci amet, iaculis libero. 
-                                    </div>
-                                    <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                        Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id dolor consectetur fermentum 
-                                        volutpat nibh, accumsan purus. Lorem ipsum dolor sit semper amet, consectetur adipiscing elit. 
-                                        In maximus ligula metus pellentesque mattis. 
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <!-- End Service Item -->
                         
                     </div>
                     <!-- End Tab panes -->

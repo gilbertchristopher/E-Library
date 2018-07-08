916067cdcc0d22e01b3374dd6d4cedf51de5d449
Nathaniel <nathanielsuhardiman@gmail.com>
2018-07-09 03:17:56 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -471,26 +471,29 @@ class MainController extends CI_Controller {
 
 	public function do_upload()
         {
-                $config['upload_path']          = './uploads/';
-                $config['allowed_types']        = 'gif|jpg|png';
-                //$config['max_size']             = 100;
-                //$config['max_width']            = 1024;
-                //$config['max_height']           = 768;
+				$config = array(
+					'upload_path' => "./uploads/",
+					'allowed_types' => "jpg|png|jpeg",
+					'overwrite' => TRUE,
+					'max_size' => "2048000", // Can be set to particular file size , here it is 2 MB(2048 Kb)
+					'max_height' => "768",
+					'max_width' => "1024"
+				);
 
                 $this->load->library('upload', $config);
 
-                if ( ! $this->upload->do_upload('userfile'))
-                {
-						//$error = array('error' => $this->upload->display_errors());
+                // if ( ! $this->upload->do_upload('userfile'))
+                // {
+				// 		$error = array('error' => $this->upload->display_errors());
 						
-						echo $data['upload_data']['full_path'];
-                        //$this->load->view('upload_form', $error);
-                }
-                else
-                {
-                        $data = array('upload_data' => $this->upload->data());
-						echo $data['upload_data']['full_path'];
-                        //$this->load->view('upload_success', $data);
-                }
+				// 		echo $data['upload_data']['full_path'];
+                //         $this->load->view('upload_form', $error);
+                // }
+                // else
+                // {
+                //         $data = array('upload_data' => $this->upload->data());
+
+                //         $this->load->view('upload_success', $data);
+                // }
         }
 }

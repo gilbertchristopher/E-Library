0269a9c0e1a264e1346be5aca6053c6561a403a2
Nathaniel <nathanielsuhardiman@gmail.com>
2018-07-08 23:05:50 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/views/pages/adminPage.php
+++ application/views/pages/adminPage.php
@@ -217,11 +217,8 @@
                                                 for($i = 0;$i < 20;$i++){ //sementara maks tampil 20, pagination blm jalan soale
                                                     echo '<div class="col-md-3 col-lg-3 mb-60 mb-xs-40">';
                                                         echo '<div class="post-prev-img">';
-                                                        ?>
-                                                        
-                                                        <a href="<?php echo $searchres[$i]->imgUrl; ?>" class="lightbox-gallery-3 mfp-image"><img style="width: 350px; height: 365px" src="<?php echo $searchres[$i]->imgUrl; ?>" alt="" /></a>
-                                                            <?php
-                                                            //echo '<a href="$searchres[$i]->imgUrl;"><img class="lightbox-gallery-3 mfp-image" style="width: 350px; height: 365px;" src=".$searchres[$i]->imgUrl." alt="" /></a>';
+                                                            echo '<a href="'.$searchres[$i]->imgUrl.'" class="lightbox-gallery-3 mfp-image"><img src="'.$searchres[$i]->imgUrl.'" alt="" style="width: 350px; height: 365px;"/></a>';
+                                                            //echo "<a href='shop-single.html'><img class='image' style='width: 350px; height: 365px;' src='".$searchres[$i]->imgUrl."' alt='' /></a>";
                                                         echo '</div>';
                                                         
                                                 ?>
